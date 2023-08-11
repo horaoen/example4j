a1705bf3c3eca389f92667c781d48b61379f21d1
叶计义 <horaoen@gmail.com>
2023-08-12 02:16:27 +0800

    update code

--- jooq/src/test/java/com/horaoen/example4j/jooq/JooqApplicationTests.java
+++ jooq/src/test/java/com/horaoen/example4j/jooq/JooqApplicationTests.java
@@ -1,13 +1,32 @@
 package com.horaoen.example4j.jooq;
 
+import com.horaoen.example4j.jooq.database.tables.Author;
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JooqApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
+@RequiredArgsConstructor
+class JooqApplicationTests {
+    @Autowired
+    private DSLContext create;
     @Test
     void contextLoads() {
+        assertNotNull(create);
+    }
+    
+    @Test
+    void insertOneTest() {
+        int inserted = create.insertInto(Author.AUTHOR).values(1, "horaoen", "fhr").execute();
+        assertEquals(1, inserted);
+        // clean
+        int deleted = create.delete(Author.AUTHOR).where(Author.AUTHOR.ID.eq(1)).execute();
+        assertEquals(1, deleted);
     }
 
 }
